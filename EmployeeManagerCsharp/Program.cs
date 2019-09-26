830d1ec29a703530a1c8319e36fa310a120be49f
David <20023188@tafe.wa.edu.au>
2019-09-26 11:55:53 +0800

    Wrote a program to test code

--- EmployeeManagerCsharp/Program.cs
+++ EmployeeManagerCsharp/Program.cs
@@ -10,6 +10,39 @@ namespace EmployeeManagerCsharp
     {
         static void Main(string[] args)
         {
+            // Create an employee
+            Employee emp1 = new Employee("Winn Dough", 13521);
+            Console.WriteLine(emp1.printDetails());
+
+            // Modify an employee
+            emp1.name = "Winny Dough";
+            emp1.number = 11;
+            Console.WriteLine(emp1.printDetails());
+
+            // Create more employees
+            Employee emp2 = new Employee("Jane Doe", 21434);
+            Employee emp3 = new Employee("Bob Hope", 2534);
+
+            // Create a manager
+            Manager man1 = new Manager("Martin", 101);
+            Console.WriteLine(man1.printDetails());
+
+            // Create a manager with employess
+            List<Employee> ItTeam = new List<Employee>();
+            ItTeam.Add(emp1);
+            ItTeam.Add(emp3);
+            Manager man2 = new Manager("Marvin", 102, ItTeam);
+            Console.WriteLine(man2.printDetails());
+
+            // Add employee to a manger
+            man1.addEmployee(emp2);
+            Console.WriteLine(man1.printDetails());
+
+            // Remove an employee from a manager
+            man2.removeEmployee(emp3);
+            Console.WriteLine(man2.printDetails());
+
+            Console.ReadLine();// Prevent closing early
         }
     }
 }
