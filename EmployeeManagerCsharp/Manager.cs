fa059d8aeced3703a994bc6361bf126f8ffe800a
David <20023188@tafe.wa.edu.au>
2019-09-26 11:26:06 +0800

    Created Manager class

---
+++ EmployeeManagerCsharp/Manager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagerCsharp
+{
+    class Manager : Employee
+    {
+        private List<Employee> employees;
+    }
+}
