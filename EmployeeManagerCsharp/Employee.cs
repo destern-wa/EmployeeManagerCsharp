fe1d5a70cf3ba99a7163b1fbbe102dbcca63e965
David <20023188@tafe.wa.edu.au>
2019-09-26 11:32:10 +0800

    Added constructor and methods to Employee class

--- EmployeeManagerCsharp/Employee.cs
+++ EmployeeManagerCsharp/Employee.cs
@@ -8,7 +8,30 @@ namespace EmployeeManagerCsharp
 {
     class Employee
     {
-        protected String name;
-        protected int number;
+        protected String _name;
+        protected int _number;
+
+        public Employee(String name, int number)
+        {
+            this.name = name;
+            this.number = number;
+        }
+
+        public String name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int number
+        {
+            get { return _number; }
+            set { _number = value; }
+        }
+
+        public String printDetails()
+        {
+            return _name + " / " + _number;
+        }
     }
-}
+    }
