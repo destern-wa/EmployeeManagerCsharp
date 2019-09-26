f2ee6669d7118572b77b30a5127f026f594f1d85
David <20023188@tafe.wa.edu.au>
2019-09-26 11:49:45 +0800

    Added constructors and methods to Manager class

--- EmployeeManagerCsharp/Manager.cs
+++ EmployeeManagerCsharp/Manager.cs
@@ -8,6 +8,45 @@ namespace EmployeeManagerCsharp
 {
     class Manager : Employee
     {
-        private List<Employee> employees;
+        private List<Employee> _employees;
+
+        public Manager(string name, int number) : base(name, number)
+        {
+            _employees = new List<Employee>();
+        }
+        public Manager(string name, int number, List<Employee> employees) : base(name, number)
+        {
+            _employees = employees;
+        }
+
+        public List<Employee> Employees {
+            get { return _employees; }
+        }
+
+        public void addEmployee(Employee newEmployee)
+        {
+            _employees.Add(newEmployee);
+        }
+
+        public void removeEmployee(Employee oldEmployee)
+        {
+            _employees.Remove(oldEmployee);
+        }
+
+        public new string printDetails()
+        {
+            string managerDetails = "Manager " + name + " / " + number + "\n";
+            if (_employees.Count == 0)
+            {
+                return managerDetails + "(no employess)";
+            }
+            managerDetails += "Employees:";
+            foreach (Employee employee in _employees)
+            {
+                managerDetails += "\n- " + employee.printDetails();
+            }
+            return managerDetails;
+        }
     }
+
 }
