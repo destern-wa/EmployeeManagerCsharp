3e1b8ba4979a66e1e450f7bbfb336e353d66b75a
David <20023188@tafe.wa.edu.au>
2019-09-26 11:24:32 +0800

    Created Employee class

---
+++ EmployeeManagerCsharp/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagerCsharp
+{
+    class Employee
+    {
+        protected String name;
+        protected int number;
+    }
+}
